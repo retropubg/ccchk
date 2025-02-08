2e3eb55d8688fab3965f24c854a6e801f9ba029a
Retro <74841742+retropubg@users.noreply.github.com>
2025-02-08 01:30:04 -0400

    Update __init__.py

--- ubb/__init__.py
+++ ubb/__init__.py
@@ -16,5 +16,8 @@ API_HASH = os.getenv('API_HASH', CONFIG['api_hash'])
 DUMP_ID = os.getenv('DUMP_ID', CONFIG['dump_id'])
 STRING_SESSION = os.getenv('STRING_SESSION', CONFIG['string_session'])
 
-# Initialize Telegram Client
-Ubot = TelegramClient(StringSession(1AZWarzsBux0cAwohXalqVXlck2QmUAPtpPcv4bhoxuDJoHPn1sDFsDsGwH-yU4MQgE0_0kXx_OpnUhwY2ZDhkEBEUtuzm-zcPBHqMviPAxK8JJ0X1fQP6RWG3LN41Y2QeTPvh9zMe9Pb27KewmYH-WXaOf_uxrcEIhLsHnYvHGwZ-oUgq0mz_6blnkd6RJZoY28l1M41ELmTzNclA-B7BXnG40MKQMdKMvd2JaDNxkVTOEpmGGaHELcqK-86JviXUXtc7kKopXBMtQTcZ8ybmmGnzzLKAZA1WqXqHbDhEqH4gOkxG5E8e8L0Ul2IknEgqMxCVartXI-InIigIedXcpkD6kRarwE=), 26043952, 96b8dea447ef580b5b75b01ccc3ab710, auto_reconnect=False)
+# Ensure the session string is in quotes
+session_string = "1AZWarzsBux0cAwohXalqVXlck2QmUAPtpPcv4bhoxuDJoHPn1sDFsDsGwH-yU4MQgE0_0kXx_OpnUhwY2ZDhkEBEUtuzm-zcPBHqMviPAxK8JJ0X1fQP6RWG3LN41Y2QeTPvh9zMe9Pb27KewmYH-WXaOf_uxrcEIhLsHnYvHGwZ-oUgq0mz_6blnkd6RJZoY28l1M41ELmTzNclA-B7BXnG40MKQMdKMvd2JaDNxkVTOEpmGGaHELcqK-86JviXUXtc7kKopXBMtQTcZ8ybmmGnzzLKAZA1WqXqHbDhEqH4gOkxG5E8e8L0Ul2IknEgqMxCVartXI-InIigIedXcpkD6kRarwE="
+
+# Correctly initialize the TelegramClient
+Ubot = TelegramClient(StringSession(session_string), 26043952, "96b8dea447ef580b5b75b01ccc3ab710", auto_reconnect=False)
