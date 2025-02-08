eadea8e15a624efdb5bc77ca5065c2b22195fba7
Retro <74841742+retropubg@users.noreply.github.com>
2025-02-08 00:27:33 -0400

    Update __init__.py

--- ubb/__init__.py
+++ ubb/__init__.py
@@ -14,8 +14,7 @@ CONFIG = yaml.load(open('config.yml', 'r'), Loader=yaml.SafeLoader)
 API_ID = int(os.getenv('26043952', CONFIG['api_id']))
 API_HASH = os.getenv('96b8dea447ef580b5b75b01ccc3ab710', CONFIG['api_hash'])
 DUMP_ID = os.getenv('DUMP_ID', CONFIG['dump_id'])
-STRING_SESSION = os.getenv('AQGNZjAAdMbax-pTI7YPEhNmELkkfqoAFvtfIZHzgt-fSp_58726NLA70cQ9U25SwfMQBwVT829xp6xmQmxtFuV1_96c0qsjYj2HJCo25SlD54dn_Su5HDaf_OTDBsKAlW2nPo4wsOpHEbNfiEedclj9wuj6S97f3AZnajoIBP-z1Xf5-PmgUaaV6qms7rip6bLY-TjJLrtmT_R-JUcwU8dOHNHEAGVuQoRiikEex5TcVEdnXdnSBP7su6k2XcF4bJYDYq7juEa9Ovka8K_IZxbCc4Ll170bTdHt52OYafSfaX5taC_eFFWHWi1eLjxBhBB_SNeckxK9bC5sV-mKFyxnmQLlkAAAAAHP0O-hAA
-', CONFIG['string_session'])
+STRING_SESSION = os.getenv('AQGNZjAAdMbax-pTI7YPEhNmELkkfqoAFvtfIZHzgt-fSp_58726NLA70cQ9U25SwfMQBwVT829xp6xmQmxtFuV1_96c0qsjYj2HJCo25SlD54dn_Su5HDaf_OTDBsKAlW2nPo4wsOpHEbNfiEedclj9wuj6S97f3AZnajoIBP-z1Xf5-PmgUaaV6qms7rip6bLY-TjJLrtmT_R-JUcwU8dOHNHEAGVuQoRiikEex5TcVEdnXdnSBP7su6k2XcF4bJYDYq7juEa9Ovka8K_IZxbCc4Ll170bTdHt52OYafSfaX5taC_eFFWHWi1eLjxBhBB_SNeckxK9bC5sV-mKFyxnmQLlkAAAAAHP0O-hAA', CONFIG['string_session'])
 
 Ubot = TelegramClient(StringSession(STRING_SESSION),
                       API_ID,
