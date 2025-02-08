47b06141415a3cc89eed8dfc7791cd89a29c8fd5
Retro <74841742+retropubg@users.noreply.github.com>
2025-02-08 03:06:12 -0400

    Update __main__.py

--- ubb/__main__.py
+++ ubb/__main__.py
@@ -4,13 +4,17 @@ import asyncio
 
 from ubb import Ubot
 from ubb.modules import ALL_MODULES
-
+from telethon import events
 
 for module_name in ALL_MODULES:
     imported_module = importlib.import_module(f"ubb.modules.{module_name}")
 
 
-async def main():
+
+@client.on(events.NewMessage(pattern="/alive"))
+async def alive(event):
+    await event.reply("Yes, I'm alive!")
+
     async with Ubot:
         # Run the client until Ctrl+C is pressed, or the client disconnects
         print('Your bot is alive .alive to check\n'
