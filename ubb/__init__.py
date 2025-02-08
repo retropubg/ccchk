512b24dadb8a9e35105657085ed6b4d86d178743
Retro <74841742+retropubg@users.noreply.github.com>
2025-02-08 01:01:40 -0400

    Update __init__.py

--- ubb/__init__.py
+++ ubb/__init__.py
@@ -16,7 +16,7 @@ API_HASH = os.getenv('API_HASH', CONFIG['api_hash'])
 DUMP_ID = os.getenv('DUMP_ID', CONFIG['dump_id'])
 STRING_SESSION = os.getenv('STRING_SESSION', CONFIG['string_session'])
 
-Ubot = TelegramClient(StringSession('1AZWarzsBux0cAwohXalqVXlck2QmUAPtpPcv4bhoxuDJoHPn1sDFsDsGwH-yU4MQgE0_0kXx_OpnUhwY2ZDhkEBEUtuzm-zcPBHqMviPAxK8JJ0X1fQP6RWG3LN41Y2QeTPvh9zMe9Pb27KewmYH-WXaOf_uxrcEIhLsHnYvHGwZ-oUgq0mz_6blnkd6RJZoY28l1M41ELmTzNclA-B7BXnG40MKQMdKMvd2JaDNxkVTOEpmGGaHELcqK-86JviXUXtc7kKopXBMtQTcZ8ybmmGnzzLKAZA1WqXqHbDhEqH4gOkxG5E8e8L0Ul2IknEgqMxCVartXI-InIigIedXcpkD6kRarwE=,
+Ubot = TelegramClient(StringSession(1AZWarzsBux0cAwohXalqVXlck2QmUAPtpPcv4bhoxuDJoHPn1sDFsDsGwH-yU4MQgE0_0kXx_OpnUhwY2ZDhkEBEUtuzm-zcPBHqMviPAxK8JJ0X1fQP6RWG3LN41Y2QeTPvh9zMe9Pb27KewmYH-WXaOf_uxrcEIhLsHnYvHGwZ-oUgq0mz_6blnkd6RJZoY28l1M41ELmTzNclA-B7BXnG40MKQMdKMvd2JaDNxkVTOEpmGGaHELcqK-86JviXUXtc7kKopXBMtQTcZ8ybmmGnzzLKAZA1WqXqHbDhEqH4gOkxG5E8e8L0Ul2IknEgqMxCVartXI-InIigIedXcpkD6kRarwE=,
                       API_ID,
                       API_HASH,
                       auto_reconnect=False,
