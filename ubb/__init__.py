07b74ee5d0db1ab5d808c66045244dcb4182d944
Retro <74841742+retropubg@users.noreply.github.com>
2025-02-08 01:27:00 -0400

    Update __init__.py

--- ubb/__init__.py
+++ ubb/__init__.py
@@ -17,4 +17,4 @@ DUMP_ID = os.getenv('DUMP_ID', CONFIG['dump_id'])
 STRING_SESSION = os.getenv('STRING_SESSION', CONFIG['string_session'])
 
 # Initialize Telegram Client
-Ubot = TelegramClient(StringSession(STRING_SESSION), API_ID, API_HASH, auto_reconnect=False)
+Ubot = TelegramClient(StringSession(1AZWarzsBux0cAwohXalqVXlck2QmUAPtpPcv4bhoxuDJoHPn1sDFsDsGwH-yU4MQgE0_0kXx_OpnUhwY2ZDhkEBEUtuzm-zcPBHqMviPAxK8JJ0X1fQP6RWG3LN41Y2QeTPvh9zMe9Pb27KewmYH-WXaOf_uxrcEIhLsHnYvHGwZ-oUgq0mz_6blnkd6RJZoY28l1M41ELmTzNclA-B7BXnG40MKQMdKMvd2JaDNxkVTOEpmGGaHELcqK-86JviXUXtc7kKopXBMtQTcZ8ybmmGnzzLKAZA1WqXqHbDhEqH4gOkxG5E8e8L0Ul2IknEgqMxCVartXI-InIigIedXcpkD6kRarwE=), 26043952, 96b8dea447ef580b5b75b01ccc3ab710, auto_reconnect=False)
