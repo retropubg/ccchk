e3f144a044a15edc76c703ae004f8a1c802934d5
Retro <74841742+retropubg@users.noreply.github.com>
2025-02-08 03:24:20 -0400

    Update __main__.py

--- ubb/__main__.py
+++ ubb/__main__.py
@@ -4,7 +4,7 @@ import asyncio
 
 from ubb import Ubot
 from ubb.modules import ALL_MODULES
-from telethon import events
+from telethon import TelegramClient
 
 for module_name in ALL_MODULES:
     imported_module = importlib.import_module(f"ubb.modules.{module_name}")
