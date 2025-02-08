231a07bab40c71e245d2fb72ca14d76d2e1a2059
Retro <74841742+retropubg@users.noreply.github.com>
2025-02-08 03:31:36 -0400

    Update __main__.py

--- ubb/__main__.py
+++ ubb/__main__.py
@@ -11,9 +11,6 @@ for module_name in ALL_MODULES:
 
 
 
-api_id = 26043952  # Replace with your actual API ID
-api_hash = "96b8dea447ef580b5b75b01ccc3ab710"
-Ubot = TelegramClient("1AZWarzoBuxcfU1NuhKl6b6K_4yM7J5Pl9rUz_VZWjOJIuCxpRN2RLk2FYcs6T84WqMIZOmP7GWfQV1AmgSnkifVmg8yFGt05dfQpWq88TJuEZHktJPQntcAp37It0PFMhbBZzqgKApJW2IfW-P5ZyoZ33BswDtI5jv9qBtwD7PWpZ4GZzYvAGfTGDcjxfKVYZID74JgNwvdAU1Y8Bg3H_5e3elLGN4HxpAo05IgbA3YldU7M7zjwU2HTqpAlAphGQ7UdEfSkWn9sK93G03JskQXtkHSeRkLP8K10S836ZbWeZZ9DVGhyXJ5nIjbJIA-9j0i-MZClr0qSxY_rFmP9db3SZuDl7Sc=", api_id, api_hash)
 
 async def main():
     await Ubot.start()
